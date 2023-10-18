41d2107605e52e865a6942f49c00885560c0a052
LAPTOP-SQ03A8DH\Amber <Amberqu.q2@gmail.com>
2023-10-18 00:08:52 -0400

    functioning scene changes and added non-functioning oxygen level timer

---
+++ Assets/Scripts/timer.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class timer : MonoBehaviour
+{
+    Image timerBar;
+    public float maxTime = 5f;
+    float timeLeft;
+
+    void Start ()
+    {
+        timerBar = GetComponent<Image>();
+        timeLeft = maxTime;
+    }
+
+    void Update()
+    {
+        if (timeLeft > 0)
+        {
+            timeLeft -= Time.deltaTime;
+            timerBar.fillAmount = timeLeft / maxTime;
+        }
+        else
+        {
+            SceneManager.LoadScene("Main");
+            Time.timeScale = 0;
+        }
+
+    }
+}
